4f7b86ef49c8cad508a0f41b958d6a60fb0e59d1
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-06-21 15:04:24 +0800

    additional baby

--- cogs/auto_responses.py
+++ cogs/auto_responses.py
@@ -217,8 +217,18 @@ class Auto_Responses(commands.Cog):
             # how are you
             else:
                 if reply_message.content.lower().startswith("how are you"):
+                    feelings =[
+                        "I'm okay, thank you.",
+                        "i'm fine, thank you.",
+                        "Not good",
+                        "Go away",
+                        "Not okay",
+                        "I'm good!",
+                        "I am not okay",
+                        "Go bother someone else"
+                    ]
                     await reply_message.channel.trigger_typing()
-                    await reply_message.channel.send("I'm okay, thank you.")
+                    await reply_message.channel.send(f'{random.choice(feelings)}')
 
                 # tell me about yourself
                 if reply_message.content.lower().startswith("tell me about yourself"):
@@ -239,7 +249,7 @@ class Auto_Responses(commands.Cog):
                     await reply_message.channel.send(embed=embed)
 
                 # Isaac Asimov Robot Laws
-                if reply_message.content.lower().startswith("tell me about robot laws") or reply_message.content.lower().startswith("tell me about the robot laws") or reply_message.content.lower().replace("'", "").strip().startswith("tell me about isaac asimovs robot laws"):
+                if reply_message.content.lower().startswith("tell me about robot laws") or reply_message.content.lower().startswith("tell me about the robot laws"):
                     embed = discord.Embed (
                         title="The Three Laws of Robotics",
                         color=0xc7ecf7
