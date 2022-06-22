8f03e811e82e07be16f798a6afec0ec02ad8bbc3
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-06-22 18:25:40 +0800

    Update bot_statuses.py

--- cogs/bot_statuses.py
+++ cogs/bot_statuses.py
@@ -12,10 +12,12 @@ class Bot_Statuses(commands.Cog):
     @tasks.loop(seconds=999.0)
     async def random_status_loop(self):
         status = [
-            "Splatoon 3",
-            "Nintendo Switch Sports",
-            "Animal Crossing New Horizons",
-            "Taiko no Tatsujin: Drum 'n' Fun",
+            "Elden Ring",
+            "Biomutant",
+            "Disney Mirrorverse",
+            "VALORANT",
+            "Overwatch",
+            "Genshin Impact",
             "The Stanley Parable",
             "The Stanley Parable Ultra Deluxe",
             "The Stanley Parable 2",
@@ -32,21 +34,13 @@ class Bot_Statuses(commands.Cog):
             "The Journey Down Chapter 1",
             "The Journey Down Chapter 2",
             "The Journey Down Chapter 3",
-            "Flipping Death",
-            "Hollow Knight",
-            "Shovel Knight",
             "Persona 3",
             "Persona 3 FES",
             "Persona 3 Portable",
             "Persona 4",
             "Persona 4 Golden",
             "Persona 5",
-            "Persona 5 Royal",
-            "Worms 4 Mayhem",
-            "Worms Ultimate Mayhem",
-            "MapleStory",
-            "Club Penguin",
-            "Club Penguin Rewritten"
+            "Persona 5 Royal"
         ]
 
         await self.client.change_presence (
