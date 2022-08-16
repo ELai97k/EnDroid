8f0858b4ade2f29f468a13454343df20d2c17123
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-16 23:13:56 +0800

    Update auto_responses.py

--- cogs/auto_responses.py
+++ cogs/auto_responses.py
@@ -396,28 +396,7 @@ class Auto_Responses(commands.Cog):
                             await message.channel.trigger_typing()
                             return await reply_message.channel.send(response)
                             
-        # e
-        if "e" in message.content and "!" not in message.content and message.author.id != 696008187991687189 and message.author.id != 973407928654651392:
-            await asyncio.sleep(20)
-            e_responses = [
-                "what's going on here?",
-                "wdym",
-                "I see",
-                "Oh",
-                "yes",
-                "no",
-                "What?",
-                "Who?",
-                "Why?",
-                "When?",
-                "How?",
-                "ah",
-                "ok",
-                "Okay",
-                "I don't get it"
-            ]
-            await message.channel.send(f'{random.choice(e_responses)}')
-            print("e")
+       
 
         # turn that frown upside down
         if ":(" in message.content:
