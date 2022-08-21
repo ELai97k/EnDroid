641435122120f6fe55ffeb6083763efb06fce52a
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-21 21:55:11 +0800

    Update clearchats.py

--- cogs/clearchats.py
+++ cogs/clearchats.py
@@ -1,25 +1,21 @@
+from ast import alias
 import discord
 from discord.ext import commands
 
-class ClearChats(commands.Cog):
+class Clear_Chats(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(pass_context=True, aliases=["deletechats", "clearmsgs", "deletemsgs"])
-    @commands.has_role("Moderators")
-    async def clearchats(self, ctx, amount=None):
-        if amount is None:
-            print(f"Deleting {amount} messages")
-            await ctx.channel.purge(limit=50)
-
-        elif amount == "all":
-            print("Purging text channel . . .")
-            await ctx.channel.purge()
-
-        else:
-            print(f"Deleting {amount} messages")
-            await ctx.channel.purge(limit=int(amount))
+    # clear chat in text channel
+    @commands.command(pass_context=True, name="clearchats", aliases=["deletechats"])
+    async def _clearchats(self, ctx, amount=100):
+        channel = ctx.message.channel
+        messages = []
+        async for message in self.client.logs_from(channel, limit=int(amount) + 1):
+            messages.append(message)
+            await self.client.delete_messages(messages)
+            print(f"Deleting messages in {channel} . . .")
 
 
 def setup(client):
-    client.add_cog(ClearChats(client))
+    client.add_cog(Clear_Chats(client))
