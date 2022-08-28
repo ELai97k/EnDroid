b36a7f7f20310c3f861e0469a8d51ab62d41098f
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-29 01:44:24 +0800

    Create admin.py

---
+++ cogs/admin.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+
+class Admin(commands.Cog):
+    """Kick and ban command for admin and mods"""
+    def __init__(self, client):
+        self.client = client
+
+    # kick command
+    @commands.command(pass_context=True)
+    @commands.has_role("Moderators")
+    @has_permissions(manage_roles=True, kick_members=True)
+    async def kick(self, ctx, member:discord.Member, *, reason=None):
+        if member.guild_permissions.manage_roles:
+            embed = discord.Embed (
+                title = "Command Error!",
+                description = "Admin or Moderators cannot be kicked!",
+                color = discord.Color.dark_red()
+            )
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed (
+                title = "Operation Successful!",
+                description = f"{member.name} has been kicked from this server.",
+                color=0x198C19
+            )
+            embed.add_field (
+                name = "Reason:",
+                value = f"{reason}",
+                inline=False
+            )
+            await member.kick(reason=reason)
+            await ctx.send(embed=embed)
+            print(f"{member.name} has been kicked from the server.")
+
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You do not have permission to use this command!")
+
+
+    # ban command
+    @commands.command(pass_context=True)
+    @commands.has_role("Moderators")
+    @has_permissions(manage_roles=True, ban_members=True)
+    async def ban(self, ctx, member:discord.Member, *, reason=None):
+        if member.guild_permissions.manage_roles:
+            embed = discord.Embed (
+                title = "Command Error!",
+                description = "Admin or Moderators cannot be banned!",
+                color = discord.Color.dark_red()
+            )
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed (
+                title = "Operation Successful!",
+                description = f"{member.name} has been banned from this server.",
+                color=0x198C19
+            )
+            embed.add_field (
+                name = "Reason:",
+                value = f"{reason}",
+                inline=False
+            )
+            await member.ban(reason=reason)
+            await ctx.send(embed=embed)
+            print(f"{member.name} has been banned from the server.")
+
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You do not have permission to use this command!")
+
+
+def setup(client):
+    client.add_cog(Admin(client))
