32e45c7dba6ca02b93a995e49bdb101f05692cfc
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-30 14:25:31 +0800

    Update stanley.py

--- cogs/stanley.py
+++ cogs/stanley.py
@@ -26,26 +26,8 @@ class Stanley(commands.Cog):
 
 
     # Stanley command
-    @commands.command()
+    @commands.command(aliases=["employee427", "stanleyparable"])
     async def stanley(self, ctx):
-        if ctx.author == self.client.user:
-            return
-        if ctx.author.bot:
-            return
-
-        embed = discord.Embed (
-            title = "The Stanley Parable",
-            description = "This is the story of a man named Stanley. Stanley worked for a company in a big building where he was Employee Number 427. Employee Number 427's job was simple, he sat at his desk in Room 427 and he pushed buttons on a keyboard. Orders came to him through a monitor on his desk, telling him what buttons to push, how long to push them, and in what order. This is what Employee 427 did every day, of every month, of every year. And although others might have considered it soul-rending, Stanley relished every moment that the orders came in, as though he had been made exactly for this job. And Stanley was happy.",
-            color=0xe4d21d
-        )
-        embed.set_footer(text = "Original Story by The Narrator\nWritten by Davey Wreden")
-        await ctx.send(embed=embed)
-        print("The Stanley Parable")
-
-
-    # Employee 427 command
-    @commands.command()
-    async def employee427(self, ctx):
         if ctx.author == self.client.user:
             return
         if ctx.author.bot:
