5275b12a315d4b48eff7a4caf9913df8ad9db772
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-31 18:47:11 +0800

    Update g_bar.py

--- cogs/g_bar.py
+++ cogs/g_bar.py
@@ -11,7 +11,7 @@ class g_bar(commands.Cog):
         if member.guild.name == "Gay Bar 3.0":
             embed = discord.Embed (
                 title=f'Hello {member.name}, and welcome to **{member.guild.name}**!',
-                description='Have a look at the rules at <#855777347730407434> and server info at <#855775497955180545>. Once again, welcome to gay bar!',
+                description='Have a look at the rules at <#855777347730407434> and server info at <#855775497955180545>.',
                 color=0x198C19
             )
             embed.set_footer(text='Thank you for joining us! We hope you enjoy your stay.')
