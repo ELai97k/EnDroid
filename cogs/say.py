555d240153fca1217059200e1d49654c0a525a39
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-08-31 16:25:19 +0800

    Update say.py

--- cogs/say.py
+++ cogs/say.py
@@ -10,7 +10,7 @@ class Say(commands.Cog):
     # say command
     @commands.command()
     @has_permissions(administrator=True)
-    async def say(self, ctx, *, message=None):
+    async def say(self, ctx, message=None):
         if ctx.author == self.client.user:
             return
         if ctx.author.bot:
