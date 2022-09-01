7a97e632d4a0ea8998a5eb009cbe29e08f11758d
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-01 14:34:20 +0800

    Update triggers.py

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -1,73 +1,13 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions, MissingPermissions
 import datetime
-
-import json
-with open('reports.json', encoding='utf-8') as f:
-  try:
-    report = json.load(f)
-  except ValueError:
-    report = {}
-    report['users'] = []
 
 
 class Triggers(commands.Cog):
+    """Bad words filters"""
     def __init__(self, client):
         self.client = client
 
-
-    # swear words
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.client.user:
-            return
-        if message.author.bot:
-            return
-
-        bad_list = [
-            "dick",
-            "cuck",
-            "cock",
-            "sex",
-            "nigga",
-            "nigger",
-            "negro",
-            "penis",
-            "vagina",
-            "nude",
-            "porn",
-            "pornhub.com",
-            "brazzers.com",
-            "faggot",
-            "naked",
-            "cum ",
-            "motherfucker"
-        ]
-        for bad_word in bad_list:
-            with open('reports.json', 'r') as f:
-                swear_data = json.load(f)
-            swear_data[str(message.author.id)]
-
-            if bad_word in message.content.lower():
-
-                # warning embed
-                embed = discord.Embed (
-                    title=f"**⚠ WARNING for {message.author.name}!**",
-                    description="You have broken one of Da Rules in this server. If you receive too many warnings, you will be either **kicked or banned**.",
-                    color=discord.Color.dark_orange()
-                )
-                embed.set_footer(text="If you think that this was a mistake, pls ping the Admin or Mods for further discussion")
-                embed.timestamp = datetime.datetime.utcnow()
-                await message.channel.send(embed=embed)
-                await message.delete()
-                print(f"{message.author} has been given a warning!")
-
-            swear_data[str(message.author.id)]
-            with open('reports.json', 'w') as f:
-                json.dump(swear_data, f)
-
-    
     # trigger words
     @commands.command()
     async def triggers(self, ctx):
@@ -83,6 +23,48 @@ class Triggers(commands.Cog):
         )
         embed.set_footer(text="You will be given a warning that will trigger Endroid's auto-moderation")
         await ctx.send(embed=embed)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            return
+
+        # bad words
+        if not message.author.bot:
+            bad_list = [
+                "dick",
+                "cuck",
+                "cock",
+                "sex",
+                "nigga",
+                "nigger",
+                "negro",
+                "penis",
+                "vagina",
+                "nude",
+                "porn",
+                "pornhub.com",
+                "brazzers.com",
+                "faggot",
+                "naked",
+                "cum ",
+                "motherfucker"
+            ]
+            for bad_word in bad_list:
+                if bad_word in message.content.lower():
+                    # trigger embed
+                    embed = discord.Embed (
+                        title=f"**⚠ WARNING for {message.author.name}!**",
+                        description="You have broken one of Da Rules in this server. If you receive too many warnings, you will be either **kicked or banned**.",
+                        color=discord.Color.dark_orange()
+                    )
+                    embed.set_footer(text="If you think that this was a mistake, pls DM or ping the Admin or Mods for further discussion")
+                    embed.timestamp = datetime.datetime.utcnow()
+
+                    await message.channel.send(embed=embed)
+                    await message.delete()
+                    print(f"{message.author} has been given a warning!")
 
 
 def setup(client):
