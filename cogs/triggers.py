9c4847af75dca07287f86f4c2011b6d387af4a72
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-02 21:36:34 +0800

    Update triggers.py

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime
 
 class Triggers(commands.Cog):
-    """Bad words filters"""
+    """Bot filter for bad words"""
     def __init__(self, client):
         self.client = client
 
