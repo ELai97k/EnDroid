0253e8978f44fa62fb41175605d3dec87fdafb65
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-03 00:23:28 +0800

    Create heyendroid.py

---
+++ cogs/heyendroid.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+
+class HeyPrompts(commands.Cog):
+    """'hey endroid' prompts and responses"""
+    def __init__(self, client):
+        self.client = client
+
+    # embed
+    @commands.command()
+    async def heyprompts(self, ctx):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        embed = discord.Embed (
+            title = "List of available options when typing 'Hey Endroid':",
+            color=0xc7ecf7
+        )
+        # how are you
+        embed.add_field(name="Ask how is Endroid", 
+        value="Prompts: 'how are you', 'how are you doing'",
+        inline=False)
+
+        # features
+        embed.add_field(name="Ask about Endroid's functions / features", 
+        value="Prompts: 'what can you do', 'what are your features', 'what are your functions'",
+        inline=False)
+
+        # tell me about yourself
+        embed.add_field(name="Ask Endroid to introduce himself", 
+        value="Prompts: 'tell me about yourself', 'who are you', 'what are you'",
+        inline=False)
+
+        # what are you doing right now
+        embed.add_field(name="Ask Endroid what he's doing", 
+        value="Prompts: 'what are you doing right now', 'what are you doing now', 'what are you doing', 'what you doing', 'whatcha doin', 'whatcha doing', 'what'cha doin', 'what'cha doing', 'what are you doing currently', 'what are you currently doing'",
+        inline=False)
+
+        # talk to me
+        embed.add_field(name="Make Endroid talk to you", 
+        value="Prompts: 'talk to me', 'talk to me pls', 'pls talk to me', 'can you talk to me', 'can you pls talk to me', 'can ypu talk to me pls'",
+        inline=False)
+        
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(HeyPrompts(client))
