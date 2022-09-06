67c8f569aa173ca36707477249cdc735cd9309a1
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-06 21:47:05 +0800

    Update warnings.py

--- cogs/warnings.py
+++ cogs/warnings.py
@@ -3,14 +3,6 @@ import json
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
-with open('warns.json', encoding='utf-8') as f:
-    try:
-        report = json.load(f)
-    except ValueError:
-        report = {}
-        report['users'] = []
-
-
 class Warnings(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -19,6 +11,13 @@ class Warnings(commands.Cog):
     @commands.has_role("Moderators")
     @commands.has_permissions(manage_roles=True, kick_members=True, ban_members=True)
     async def warn(self, ctx, user:discord.User, *reason:str):
+        with open('warns.json', encoding='utf-8') as f:
+            try:
+                report = json.load(f)
+            except ValueError:
+                report = {}
+                report['users'] = []
+
         if ctx.author == self.client.user:
             return
         if ctx.author.bot:
