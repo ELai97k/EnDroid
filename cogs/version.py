912e283321704a12ab4a73905cd172d3eb44dc05
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-08 18:05:02 +0800

    Update version.py

--- cogs/version.py
+++ cogs/version.py
@@ -7,9 +7,18 @@ class Version(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def version(ctx, version):
-        version = "python-3.10.7"
+    async def version(self, ctx, version):
+        version = "Python version: 3.10.7"
         await ctx.send(version)
+
+    @commands.command()
+    async def botver(self, ctx):
+        embed = discord.Embed (
+            title = "Python version",
+            description = "```python-3.10.7```",
+            color=0xc7ecf7
+        )
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Version(client))
