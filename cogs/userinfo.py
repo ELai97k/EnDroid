c260c933bd6fafaabb38255bfa837e51091a32e3
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-08 23:17:29 +0800

    Update userinfo.py

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -16,35 +16,51 @@ class UserInfo(commands.Cog):
         
         if user is None:
             user = ctx.author
-
+        
+        # day, date, year
         date_format = "%a, %d %b, %Y"
+
+        # discord status
         status = user.status[0]
+
+        # discord status names
         statusnames = {"online" : "Online", "dnd" : "Do Not Disturb", "idle" : "Idle", "offline" : "Invisible/Offline"}
+
+        # discord status emojis
         statusemojis = {"online" : "🟢", "dnd": "🔴", "idle" : "🌙", "offline" : "⚫"}
+
+        # server roles
+        role_string = ' '.join([r.mention for r in user.roles][1:])
+
+        # server / guild permissions
+        perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+
+        # server join position
+        #members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
 
         embed = discord.Embed (
             title = f"{user.name}'s Info",
-            color=0xffd966
+            color=0xc7ecf7
         )
         embed.set_author(name=str(user), icon_url=user.avatar_url)
         embed.set_thumbnail(url=user.avatar_url)
 
         # server join date
         embed.add_field(name="Date joined:", value=user.joined_at.strftime(date_format), inline=False)
+        
         # discord account created date
         embed.add_field(name="Account created:", value=user.created_at.strftime(date_format), inline=False)
 
         # server join position
-        #members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
         #embed.add_field(name="Join position:", value=str(members.index(user)+1), inline=False)
+        
+        # server roles
+        embed.add_field(name="Server roles ({})".format(len(user.roles)-1), value=role_string, inline=False)
 
-        if len(user.roles) > 1:
-            role_string = ' '.join([r.mention for r in user.roles][1:])
-            embed.add_field(name="Server roles ({})".format(len(user.roles)-1), value=role_string, inline=False)
-
-        perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
-
+        # server / guild permissions
         embed.add_field(name="Guild permissions:", value=perm_string, inline=False)
+
+        # member status
         embed.add_field(name="Status:", value=f"{statusemojis[status]} {statusnames[status]}", inline=False)
 
         embed.timestamp = datetime.datetime.utcnow()
