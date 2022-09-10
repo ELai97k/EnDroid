849546f4e9f37644cd748274229e08fc71d1ebcc
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-10 10:03:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 import json
 from discord.ext import commands
-# import customhelpcommand.py from cogs folder
+# import customhelp.py from cogs folder
 from cogs.customhelpcommand import CustomHelpCommand
 
 intents = discord.Intents.default().all()
