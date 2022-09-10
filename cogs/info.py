508e382df89491078eabc036372d83ca302234c1
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-10 10:15:04 +0800

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -19,7 +19,7 @@ class Info(commands.Cog):
             color=0xc7ecf7
         )
         embed.add_field(name="info", 
-        value="Alternate help command that you're looking at right now.",
+        value="Custom help command that you're looking at right now.",
         inline=False)
 
         embed.add_field(name="help", 
