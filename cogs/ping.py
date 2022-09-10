020061b7ca9409f716b4a273a209b63bfc995818
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-10 10:17:10 +0800

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext import commands
 
 class Ping(commands.Cog):
-    """Ping command that shows bot latency"""
+    """Bot ping cog."""
     def __init__(self, client):
         self.client = client
 
     # ping command
-    @commands.command()
+    @commands.command(help="Command that shows the bot's latency.")
     async def ping(self, ctx):
         if ctx.author == self.client.user:
             return
