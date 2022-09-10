3da2dc3a2c349857bccd9ad26ec198ad45a68e14
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-10 10:26:55 +0800

    Update say.py

--- cogs/say.py
+++ cogs/say.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
 class Say(commands.Cog):
-    """Control the bot to say anything"""
+    """Control the bot to say anything."""
     def __init__(self, client):
         self.client = client
 
-    # say command
-    @commands.command()
+    # say command for admin only
+    @commands.command(help="Make the bot say your message input.")
     @has_permissions(administrator=True)
     async def say(self, ctx, *, message=None):
         if ctx.author == self.client.user:
@@ -20,6 +20,7 @@ class Say(commands.Cog):
             await ctx.send("What do you want me to say?")
 
         else:
+            # main server general channel 1010841256294875218
             await self.client.get_channel(1010841256294875218).send(f"{message}")
             await ctx.message.delete()
 
@@ -27,6 +28,32 @@ class Say(commands.Cog):
     async def say_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send("You do not have permission to use this command!")
+
+
+    # speak command for moderators
+    @commands.command(help="Make the bot say your message input.")
+    @commands.has_role("Moderators")
+    @has_permissions(manage_messages=True)
+    async def speak(self, ctx, *, message=None):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        if message == None:
+            await ctx.send("What do you want me to say?")
+
+        else:
+            # main server general channel 1010841256294875218
+            # G Bar general channel 762317365970468877
+            await self.client.get_channel(762317365970468877).send(f"{message}")
+            await ctx.message.delete()
+
+    @speak.error
+    async def speak_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You do not have permission to use this command!")
+
 
 def setup(client):
     client.add_cog(Say(client))
