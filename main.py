5d3ee033eb74b972b352cdffcfe5ea2dc7338a6d
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-15 21:33:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,59 +36,7 @@ async def on_ready():
     )
 
 
-# on message event
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.author.bot:
-        return
 
-    # shut up bot
-    if "shut up bot" in message.content.lower() or "shut up endroid" in message.content.lower() or "endroid shut up" in message.content.lower() or "bot shut up" in message.content.lower():
-        await message.channel.send("Okay :(")
-        client.unload_extension("cogs.auto_responses")
-        print("Auto Responses has been turned off")
-        await asyncio.sleep(999)
-        client.load_extension("cogs.auto_responses")
-        print("Auto Responses has been turned on")
-    
-    await client.process_commands(message)
-
-
-# auto responses off
-@client.command(pass_context=True)
-async def autoresponse_off(ctx):
-    if ctx.author == client.user:
-        return
-    if ctx.author.bot:
-        return
-
-    embed = discord.Embed (
-        title = "Operation Successful!",
-        description = "`Auto-Responses` has been turned **OFF** and your changes were saved.",
-        color=0x198C19
-    )
-    await ctx.send(embed=embed)
-    client.unload_extension("cogs.auto_responses")
-    print("Auto responses has been turned off")
-
-# auto responses on
-@client.command(pass_context=True)
-async def autoresponse_on(ctx):
-    if ctx.author == client.user:
-        return
-    if ctx.author.bot:
-        return
-
-    embed = discord.Embed (
-        title = "Operation Successful!",
-        description = "`Auto-Responses` has been turned **ON** and your changes were saved.",
-        color=0x198C19
-    )
-    await ctx.send(embed=embed)
-    client.load_extension("cogs.auto_responses")
-    print("Auto responses has been turned on")
 
 
 client.run(os.getenv('TOKEN'))
