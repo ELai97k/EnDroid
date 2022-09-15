e9d4823ff0ea9fa25f96f554dfae4f562beec9d9
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-15 13:51:57 +0800

    Update triggers.py

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -66,5 +66,5 @@ class Triggers(commands.Cog):
                     print(f"{message.author} has been given a warning!")
 
 
-async def setup(client):
+def setup(client):
     client.add_cog(Triggers(client))
