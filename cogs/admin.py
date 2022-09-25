4f0b6c02e497aa4d1e9fc34ab650b4ed737a7be7
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-25 16:37:40 +0800

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,11 +1,61 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
+from datetime import datetime
+
+import json
+with open('reports.json', encoding='utf-8') as f:
+  try:
+    report = json.load(f)
+  except ValueError:
+    report = {}
+    report['users'] = []
+
 
 class Admin(commands.Cog):
     """Kick and ban commands for admin and mods."""
     def __init__(self, client):
         self.client = client
+
+    # warn command
+    @commands.command(pass_context = True)
+    @commands.has_role("Moderators")
+    @has_permissions(manage_roles=True, kick_members=True, ban_members=True)
+    async def warn(self, ctx, user:discord.User, *reason:str):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        if not reason:
+            await ctx.send("Please provide a reason for warning.")
+            return
+
+        reason = ' '.join(reason)
+        for current_user in report['users']:
+            if current_user['name'] == user.name:
+                current_user['reasons'].append(reason)
+                break
+        else:
+            report['users'].append({
+                'name': user.name,
+                'reasons': [reason, ]
+            })
+            
+        with open('reports.json','w+') as f:
+            json.dump(report, f)
+        
+        # embed
+        embed = discord.Embed (
+            title=f"**⚠ WARNING for {user.name}!**",
+            description="You have broken one of Da Rules and your warning has been recorded. If you receive too many warnings, you will be either **kicked or banned**.",
+            color=discord.Color.dark_red()
+        )
+        embed.set_footer(text="If you think this was a mistake, DM or ping Admin or Mods for further discussion.")
+        embed.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed=embed)
+        print(f"{user.name} has been given a warning!")
+
 
     # kick command
     @commands.command(pass_context=True, help="Kick a member out of the server.")
