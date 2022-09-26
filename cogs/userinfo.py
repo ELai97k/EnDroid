7f5e5786a8aaea5cde9b994ed0331033b368af00
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-26 21:40:43 +0800

    Update userinfo.py

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -43,9 +43,6 @@ class UserInfo(commands.Cog):
             title = f"{user.name}'s Info",
             color=0xc7ecf7
         )
-        embed.set_author(name=str(user), icon_url=user.avatar_url)
-        embed.set_thumbnail(url=user.avatar_url)
-
         # server join date
         embed.add_field(name="Date joined:", value=user.joined_at.strftime(date_format), inline=False)
         
@@ -64,6 +61,7 @@ class UserInfo(commands.Cog):
         # member status
         embed.add_field(name="Status:", value=f"{statusemojis[status]} {statusnames[status]}", inline=False)
 
+        embed.set_thumbnail(url=user.avatar_url)
         embed.set_footer(text=f"Requested by {str(ctx.author)}")
         embed.timestamp = datetime.datetime.utcnow()
         await ctx.send(embed=embed)
