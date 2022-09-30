9958c366850b8c5a858860911460dfab8478c01b
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-09-30 19:54:46 +0800

    Update reactionroles.py

--- cogs/reactionroles.py
+++ cogs/reactionroles.py
@@ -5,77 +5,36 @@ class ReactionRoles(commands.Cog):
     """Reaction roles."""
     def __init__(self, client):
         self.client = client
-    
-    # reaction add to get "Verified" role
+
+    # reaction add
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        message_id = payload.message_id
+        guild_id = payload.guild_id 
+        guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
+        member = discord.utils.get(guild.members, id=payload.user_id)
 
-        if message_id == 1025041977361833994:
-            guild_id = payload.guild_id 
-            guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
+        if payload.message_id == 1025041977361833994:
+            if str(payload.emoji) == '☑️':
+                # Verified role
+                verified_role = discord.utils.get(payload.member.guild.roles, name="Verified")
+                # Unerified role
+                unverified_role = discord.utils.get(payload.member.guild.roles, name="Unverified")
 
-        if payload.emoji.name == '☑️':
-            verified_role = discord.utils.get(guild.roles, name='Verified')
-            unverified_role = discord.utils.get(guild.roles, name='Unverified')
-        
-        # react to get "Verified" role
-        if verified_role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(verified_role)
-                print(f"{verified_role} role given to {member}")
             else:
-                print("Member not found")
-        else:
-            print("Role not found")
+                # Verified role
+                verified_role = discord.utils.get(guild.roles, name=payload.emoji)
+                # Unerified role
+                unverified_role = discord.utils.get(guild.roles, name=payload.emoji)
+                
+            if verified_role is not None:
+                # add Verified role
+                await payload.member.add_roles(verified_role)
+                print(f"{verified_role} added to {member}")
 
-        # react to remove "Unverified" role
-        if unverified_role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(unverified_role)
-                print(f"{unverified_role} role removed from {member}")
-            else:
-                print("Member not found")
-        else:
-            print("Role not found")
-
-
-    # reaction remove to lose "Verified" role
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload):
-        message_id = payload.message_id
-
-        if message_id == 1025041977361833994:
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
-
-        if payload.emoji.name == '☑️':
-            verified_role = discord.utils.get(guild.roles, name='Verified')
-            unverified_role = discord.utils.get(guild.roles, name='Unverified')
-
-        # react to remove "Verified" role
-        if verified_role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(verified_role)
-                print(f"{verified_role} role removed from {member}")
-            else:
-                print("Member not found")
-        else:
-            print("Role not found")
-
-        # react to get "Unverified" role
-        if unverified_role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(unverified_role)
-                print(f"{unverified_role} role given to {member}")
-            else:
-                print("Member not found")
-        else:
-            print("Role not found")
+            if unverified_role is not None:
+                # remove Unverified role
+                await payload.member.remove_roles(unverified_role)
+                print(f"{unverified_role} removed from {member}")
 
 
 def setup(client):
