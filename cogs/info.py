0beb8ed9bcdb5e666ba4d99cf25a4c9837e20dbf
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-10-01 11:18:26 +0800

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -62,10 +62,6 @@ class Info(commands.Cog):
         value="Command for displaying list of options when you type 'hey endroid'.",
         inline=False)
 
-        embed.add_field(name="version",
-        value="Python version for the bot.",
-        inline=False)
-
         embed.add_field(name="userinfo",
         value="Command to fetch user information.",
         inline=False)
