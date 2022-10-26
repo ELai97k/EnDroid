f5b120c0ae381e9c6bb24c6e929e662455b0af35
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-10-26 11:31:33 +0800

    Update autoresponse.py

--- cogs/autoresponse.py
+++ cogs/autoresponse.py
@@ -73,42 +73,42 @@ class Autoresponse(commands.Cog):
         
         # how are you
         embed.add_field(name="Ask how is Endroid", 
-        value="**Prompts:** 'how are you', 'how are you doing'",
+        value="**Prompts:** ```'how are you', 'how are you doing'```",
         inline=False)
 
         # features
         embed.add_field(name="Ask about Endroid's functions / features", 
-        value="**Prompts:** 'what can you do', 'what are your features', 'what are your functions'",
+        value="**Prompts:** ```'what can you do', 'what are your features', 'what are your functions'```",
         inline=False)
 
         # tell me about yourself
         embed.add_field(name="Ask Endroid to introduce himself", 
-        value="**Prompts:** 'tell me about yourself', 'who are you', 'what are you'",
+        value="**Prompts:** ```'tell me about yourself', 'who are you', 'what are you'```",
         inline=False)
 
         # what are you doing right now
         embed.add_field(name="Ask Endroid what he's doing", 
-        value="**Prompts:** 'what are you doing right now', 'what are you doing now', 'what are you doing', 'what you doing', 'whatcha doin', 'whatcha doing', 'what'cha doin', 'what'cha doing', 'what are you doing currently', 'what are you currently doing'",
+        value="**Prompts:** ```'what are you doing right now', 'what are you doing now', 'what are you doing', 'what you doing', 'whatcha doin', 'whatcha doing', 'what'cha doin', 'what'cha doing', 'what are you doing currently', 'what are you currently doing'```",
         inline=False)
 
         # talk to me
         embed.add_field(name="Make Endroid talk to you", 
-        value="**Prompts:** 'talk to me', 'talk to me pls', 'pls talk to me', 'can you talk to me', 'can you pls talk to me', 'can ypu talk to me pls'",
+        value="**Prompts:** ```'talk to me', 'talk to me pls', 'pls talk to me', 'can you talk to me', 'can you pls talk to me', 'can ypu talk to me pls'```",
         inline=False)
         
         # time
         embed.add_field(name="Ask Endroid about the time. (in GMT+8)",
-        value="**Prompts:** 'whats the time', 'whats the time now', 'what's the time', 'what's the time now', 'what is the time', 'what is the time now', 'what time is it'",
+        value="**Prompts:** ```'whats the time', 'whats the time now', 'what's the time', 'what's the time now', 'what is the time', 'what is the time now', 'what time is it'```",
         inline=False)
 
         # Isaac Asimov's Laws of Robotics
         embed.add_field(name="Ask Endroid about the Laws of Robotics",
-        value="**Prompts:** 'tell me about robot laws', 'tell me about the robot laws', 'what are the robot laws', 'what are the laws of robotics'",
+        value="**Prompts:** ```'tell me about robot laws', 'tell me about the robot laws', 'what are the robot laws', 'what are the laws of robotics'```",
         inline=False)
 
         # you're / ur responses
         embed.add_field(name="hey endroid + you're / ur",
-        value="stupid, dumb, dum, a dumbass, useless, not helpful, not helping, a good bot, a clever bot, a cleverbot, a smart bot",
+        value="```stupid, dumb, dum, a dumbass, useless, not helpful, not helping, a good bot, a clever bot, a cleverbot, a smart bot```",
         inline=False)
 
         await ctx.send(embed=embed)
