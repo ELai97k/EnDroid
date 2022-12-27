e5853eaa8387555a84b3b752cd110deac7e99b38
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-12-27 15:29:26 +0800

    Update clearchat.py

--- cogs/clearchat.py
+++ cogs/clearchat.py
@@ -3,10 +3,11 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
 class ClearChat(commands.Cog):
+    """Cog for clearing chat command."""
     def __init__(self, client):
         self.client = client
 
-    @commands.command(pass_context=True, aliases=["deletechat"], help="Command to clear chats in a text channel.")
+    @commands.command(pass_context=True, help="Command to clear chats in a text channel.")
     @commands.has_role("Moderators")
     @has_permissions(manage_messages=True)
     async def clearchat(self, ctx, amount=None):
@@ -24,6 +25,11 @@ class ClearChat(commands.Cog):
                 await ctx.send("Please enter a valid integer as amount.")
             else:
                 await ctx.channel.purge(linit=amount)
+
+    @clearchat.error
+    async def clearchat_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You do not have permission to use this command!")
 
 
 def setup(client):
