ba407f0bd2ee9e279b72e99360a040fa342a8eb9
ELai97k <99068391+ELai97k@users.noreply.github.com>
2023-01-25 18:08:14 +0800

    Update reminder.py

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,5 +1,5 @@
+import discord
 import asyncio
-import json
 from discord.ext import commands
 
 class Reminder(commands.Cog):
@@ -8,50 +8,59 @@ class Reminder(commands.Cog):
         self.client = client
 
     # remind / reminder command
-    @commands.command(pass_context=True, help="Reminder command (input reminder text first then the time)")
-    async def remind(self, ctx, reminder=None, *times):
+    @commands.command(help="Reminder command (input time first then reminder text)", aliases = ["reminder", "remindme"])
+    async def remind(self, ctx, time, *, reminder):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
         user = ctx.author
-        if user == self.client.user:
+
+        seconds = 0
+
+        if time.lower().endswith("d"):
+            seconds += int(time[:-1]) * 60 * 60 * 24
+            counter = f"{seconds // 60 // 60 // 24} days"
+
+        if time.lower().endswith("h"):
+            seconds += int(time[:-1]) * 60 * 60
+            counter = f"{seconds // 60 // 60} hours"
+
+        elif time.lower().endswith("m"):
+            seconds += int(time[:-1]) * 60
+            counter = f"{seconds // 60} minutes"
+
+        elif time.lower().endswith("s"):
+            seconds += int(time[:-1])
+            counter = f"{seconds} seconds"
+
+        if seconds == 0:
+            embed = discord.Embed (
+                title = "Cannot process reminder",
+                description = "Please input a reminder text first followed by duration of time.",
+                color=0xc7ecf7
+            )
+            embed.add_field(name="Days", value="```d```")
+            embed.add_field(name="Hours", value="```h```")
+            embed.add_field(name="Minutes", value="```m```")
+            embed.add_field(name="Minimum duration", value="```1 minute```")
+            embed.add_field(name="Maximum duration", value="```7 days```")
+
+        elif seconds < 60:
+            embed = discord.Embed(color=0xc7ecf7)
+            embed.add_field(name="Warning", value="You have specified a duration that is too short! Minimum duration is 1 minute.")
+
+        elif seconds > 604800:
+            embed = discord.Embed(color=0xc7ecf7)
+            embed.add_field(name="Warning", value="You have specified a duration that is too long! Maximum duration is 7 days.")
+
+        else:
+            await ctx.send(f"Alright {user.mention}, I will remind you about `{reminder}` in `{counter}`.")
+            await asyncio.sleep(seconds)
+            await ctx.send(f"Hi {user.mention}, you asked me to remind you about `{reminder}` `{counter}` ago.")
             return
-        if user.bot:
-            return
-
-        with open("prefixes.json", "r") as f:
-            prefixes = json.load(f)
-
-        prefix = prefixes[str(ctx.guild.id)]
-
-        for time in times:
-            seconds = 0
-
-            # minutes
-            if time.lower().endswith("m"):
-                seconds += int(time[:-1]) * 60
-                counter = f"{seconds // 60} minutes"
-
-            # hours
-            if time.lower().endswith("h"):
-                seconds += int(time[:-1]) * 60 * 60
-                counter = f"{seconds // 60 // 60} hours"
-
-            # days
-            if time.lower().endswith("d"):
-                seconds += int(time[:-1]) * 60 * 60 * 24
-                counter = f"{seconds // 60 // 60 // 24} days"
-
-            command_prefix = prefix
-            r_command = "remind"
-            reminder = ctx.message.content.lower().replace(f"{command_prefix}", "").replace(f"{r_command}", "").replace(f"{time}", "").strip()
-            
-            if reminder is None:
-                await ctx.send("Please tell me what to remind you.")
-            if seconds == 0 or seconds > 2592000:
-                await ctx.send("Please input a reminder before you input the time in:\nm (minutes),\nh (hours),\nd (days).")
-            else:
-                await ctx.send(f"Okay {user.mention}, I will remind you about `{reminder}` in `{counter}`.")
-                await asyncio.sleep(seconds)
-                await ctx.send(f"Hey {user.mention}, you asked me to remind you about `{reminder}` `{counter}` ago.")
-                return
+        await ctx.send(embed=embed)
 
 
 def setup(client):
