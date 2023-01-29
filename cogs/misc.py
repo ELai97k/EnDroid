95bc4c9c94b565d846771a54a3a84b76a5161dbf
ELai97k <99068391+ELai97k@users.noreply.github.com>
2023-01-29 19:47:24 +0800

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,4 +1,6 @@
 import discord
+import pytz
+from datetime import datetime
 from discord.ext import commands
 
 class Misc(commands.Cog):
@@ -69,6 +71,50 @@ class Misc(commands.Cog):
 
         embed = discord.Embed(color=discord.Color.blurple())
         embed.set_image(url=user.avatar_url)
+        await ctx.send(embed=embed)
+
+
+    # GMT timezone
+    @commands.command(help="Fetch timezone in GMT +8")
+    async def gmt_time(self, ctx):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        timestamp = datetime.now()
+        gmt = pytz.timezone('Asia/Singapore')
+        embed = discord.Embed (
+            title = "Current date and time in GMT+8",
+            color=0xc7ecf7
+        )
+        embed.add_field(name="Date", value=timestamp.astimezone(gmt).strftime("%a, %d %b, %Y"), inline=False)
+        embed.add_field(name="Time", value=timestamp.astimezone(gmt).strftime("%I:%M %p"), inline=False)
+        await ctx.send(embed=embed)
+
+
+    # laws of robotics
+    @commands.command(aliases=["lawsofrobotics", "robotics", "isaacasimov", "isaac_asimov"], help="Isaac Asimov's Three Laws of Robotics")
+    async def laws_robotics(self, ctx):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        embed = discord.Embed (
+            title="The Three Laws of Robotics",
+            color=0xc7ecf7
+        )
+        # First Law
+        embed.add_field(name="First Law", value="A robot may not injure a human being or, through inaction, allow a human being to come to harm.", inline=False)
+        # Second Law
+        embed.add_field(name="Second Law", value="A robot must obey the orders given it by human beings except where such orders would conflict with the **First Law**.", inline=False)
+        # Third Law
+        embed.add_field(name="Third Law", value="A robot must protect its own existence as long as such protection does not conflict with the **First** or **Second Law**.", inline=False)
+
+        # embed footer
+        embed.set_footer(text="The Three Laws of Robotics was created by Isaac Asimov in 1942.")
+
         await ctx.send(embed=embed)
 
 
