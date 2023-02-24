32cecdadb4e8f5090b21a901382c21f25e1d1c89
ELai97k <99068391+ELai97k@users.noreply.github.com>
2023-02-24 15:09:58 +0800

    Update botstatuses.py

--- cogs/botstatuses.py
+++ cogs/botstatuses.py
@@ -12,19 +12,31 @@ class BotStatuses(commands.Cog):
     async def random_status_loop(self):
         status = [
             "myself",
+            "on Raspberry Pi 3",
             "Xbox PC Game Pass",
+            "Forza Horizon 5",
             "Persona 5 Royal",
             "Persona 4 Golden",
             "Persona 3 Portable",
-            "Hi-Fi Rush",
-            "Forza Horizon 5"
-            "Pico Neo 3",
+            "The Last of Us Part 1",
+            "SteamVR",
             "Beat Saber",
-            "Discord",
+            "Job Simulator: The 2050 Archives",
+            "Vacation Simulator",
+            "Vacation Simulator: Back To Job",
+            "Adobe Photoshop 2022",
+            "Adobe Illustrator 2022",
+            "Adobe After Effects 2022",
+            "Adobe Premiere Pro 2022",
+            "Adobe Animate 2022",
             "YouTube",
+            "GitHub",
             "Spotify",
-            "Ren'Py 8"
-            "Adobe Photoshop 2022"
+            "SoundCloud",
+            "Discord",
+            "Ren'Py",
+            "Hagar's Ordeal",
+            "I need new statuses!!"
         ]
         await self.client.change_presence (
             activity = discord.Activity (
