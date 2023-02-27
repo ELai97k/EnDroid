aabf1dc6e686bd3389f1f61af0414ee0e89e5c76
ELai97k <99068391+ELai97k@users.noreply.github.com>
2023-02-27 10:56:14 +0800

    Update botcog.py

--- cogs/botcog.py
+++ cogs/botcog.py
@@ -36,7 +36,7 @@ class Botcog(commands.Cog):
         if isinstance(error, CommandError):
             embed = discord.Embed (
                 title = "Command Error",
-                description = "Could not complete your request! Pls type properly, idiot!",
+                description = "Pls try again!\n```Could not complete your request!```",
                 color = discord.Color.dark_red()
             )
             await ctx.send(embed=embed)
