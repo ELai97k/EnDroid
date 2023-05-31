7efc25c335ddb9f869e7c505172e1b5d27d52566
Elai <laiangie97@gmail.com>
2023-05-31 17:16:10 +0800

    Update triggers.py

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -1,5 +1,4 @@
 import discord
-import sqlite3
 from discord.ext import commands
 
 class Triggers(commands.Cog):
@@ -8,7 +7,7 @@ class Triggers(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_message(self, message, ctx, user:discord.Member, *, reason:str):
+    async def on_message(self, message):
         if message.author == self.client.user:
             return
         if message.author.bot:
@@ -37,46 +36,14 @@ class Triggers(commands.Cog):
                 if bad_word in message.content.lower():
                     # trigger embed
                     embed = discord.Embed (
-                        title=f"**:warning: WARNING for `{message.author}`!**",
+                        title=f"**:warning: WARNING for `{message.author}`**",
                         description=f"{message.author.name}, you're being inappropriate! Don't say that again!",
                         color=discord.Color.dark_red()
                     )
-                    await message.channel.send(embed=embed)
                     await message.delete()
-                    await message.send(f"{message.author.mention} you have been given a warning!")
+                    await message.channel.send(embed=embed)
                     print(f"{message.author} has been given a warning!")
 
-        async def addwarn(ctx, reason, user):
-            db = sqlite3.connect('warnings.sqlite')
-            cursor = db.cursor()
-            cursor.execute(
-                """
-                INSERT INTO warns (
-                    user_id,
-                    user_name,
-                    reason,
-                    guild_id,
-                    guild_name
-                )
-                VALUES (?, ?, ?, ?, ?)
-                """, (user.id, user.name, reason, ctx.guild.id, ctx.guild.name)
-            )
-            db.commit()
-        await addwarn(ctx, reason, user)
-
-        db = sqlite3.connect('warnings.sqlite')
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM warns WHERE user_id = ? AND guild_id = ?", (user.id, ctx.guild.id))
-        data = cursor.fetchall()
-        if len(data) >= 3:
-            await user.kick(reason=f"`{user}` has reached 3 warnings.")
-            await ctx.send(f"`{user}` has reached 3 warnings and is promptly kicked from this server.")
-            print(f"{user} has reached 3 warnings and is kicked from this server.")
-
-        if len(data) == 4:
-            await user.ban(reason=f"`{user}` has reached 4 warnings.")
-            await ctx.send(f"`{user}` has reached 4 warnings and is banned from this server, goodbye.")
-            print(f"{user} has reached 4 warnings and is banned from this server.")
 
     # trigger words
     @commands.command(help="Command that displays a list of bad words.")
