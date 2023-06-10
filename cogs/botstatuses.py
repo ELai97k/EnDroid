8ca440e09120c012524585b1e4d5f2358e202bda
Elai <laiangie97@gmail.com>
2023-06-10 23:31:07 +0800

    Update botstatuses.py

--- cogs/botstatuses.py
+++ cogs/botstatuses.py
@@ -8,66 +8,37 @@ class BotStatuses(commands.Cog):
         self.random_status_loop.start()
 
     # bot statuses (will change from time to time)
-    # @tasks.loop(seconds=999.0)
-    # async def random_status_loop(self):
-    #     playing_statuses = [
-    #         "myself",
-    #         "Visual Studio Code",
-    #         "The Stanley Parable Ultra Deluxe",
-    #         "Stardew Valley",
-    #         "The Sims 3",
-    #         "The Sims 4",
-    #         "Splitgate",
-    #         "Minecraft",
-    #         "Minecraft Dungeons",
-    #         "Biomutant",
-    #         "Animal Crossing New Horizons",
-    #         "Splatoon 3",
-    #         "Persona 3",
-    #         "Persona 3 FES",
-    #         "Persona 3 Portable",
-    #         "Persona 4",
-    #         "Persona 4 Golden",
-    #         "Persona 5",
-    #         "Persona 5 Royal",
-    #         "Discord",
-    #         "GitHub",
-    #         "Ren'Py",
-    #         "YouTube",
-    #         "Spotify"
-    #     ]
-    #     await self.client.change_presence (
-    #         activity = discord.Activity (
-    #             type = discord.ActivityType.playing, name = f'{random.choice(playing_statuses)}'
-    #         )
-    #     )
-
-
     @tasks.loop(seconds=999.0)
     async def random_status_loop(self):
-        watching_statuses = [
-            "you",
-            "this server",
-            "Game Theory",
-            "Film Theory",
-            "Matatabi Movie Labo",
+        playing_statuses = [
+            "myself",
+            "Visual Studio Code",
+            "The Stanley Parable Ultra Deluxe",
+            "Stardew Valley",
+            "The Sims 3",
+            "The Sims 4",
+            "Splitgate",
+            "Minecraft",
+            "Minecraft Dungeons",
+            "Biomutant",
+            "Animal Crossing New Horizons",
+            "Splatoon 3",
+            "Persona 3",
+            "Persona 3 FES",
+            "Persona 3 Portable",
+            "Persona 4",
+            "Persona 4 Golden",
+            "Persona 5",
+            "Persona 5 Royal",
+            "Discord",
+            "GitHub",
+            "Ren'Py",
             "YouTube",
-            "Netflix",
-            "Disney+",
-            "Amazon Prime Video",
-            "Everything Everywhere All At Once",
-            "Spider-Man: Across the Spider-Verse",
-            "The Super Mario Bros. Movie",
-            "Transformers: Rise of the Beasts",
-            "The Grand Tour: A Scamdi Flick",
-            "The Grand Tour: Eurocrash",
-            "American Born Chinese",
-            "Attack On Titan",
-            "My Hero Academia"
+            "Spotify"
         ]
         await self.client.change_presence (
             activity = discord.Activity (
-                type = discord.ActivityType.watching, name = f'{random.choice(watching_statuses)}'
+                type = discord.ActivityType.playing, name = f'{random.choice(playing_statuses)}'
             )
         )
 
