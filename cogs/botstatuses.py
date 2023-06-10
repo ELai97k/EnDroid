11b2ad49bd1638d9b71a52d4149d90de31a46fc7
Elai <laiangie97@gmail.com>
2023-06-10 23:38:30 +0800

    Update botstatuses.py

--- cogs/botstatuses.py
+++ cogs/botstatuses.py
@@ -1,44 +1,61 @@
 import discord
-import random
+import asyncio
 from discord.ext import commands, tasks
 
 class BotStatuses(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.random_status_loop.start()
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # bot login
+        print(f"{self.client.user} logged in successfully!")
 
-    # bot statuses (will change from time to time)
-    @tasks.loop(seconds=999.0)
-    async def random_status_loop(self):
-        playing_statuses = [
-            "myself",
-            "Visual Studio Code",
-            "The Stanley Parable Ultra Deluxe",
-            "Stardew Valley",
-            "The Sims 3",
-            "The Sims 4",
-            "Splitgate",
-            "Minecraft",
-            "Minecraft Dungeons",
-            "Biomutant",
-            "Animal Crossing New Horizons",
-            "Splatoon 3",
-            "Persona 3",
-            "Persona 3 FES",
-            "Persona 3 Portable",
-            "Persona 4",
-            "Persona 4 Golden",
-            "Persona 5",
-            "Persona 5 Royal",
-            "Discord",
-            "GitHub",
-            "Ren'Py",
-            "YouTube",
-            "Spotify"
-        ]
         await self.client.change_presence (
             activity = discord.Activity (
-                type = discord.ActivityType.playing, name = f'{random.choice(playing_statuses)}'
+            type = discord.ActivityType.playing, name = "!help"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "Visual Studio Code"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "myself"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "discord.py"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "!help"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "Visual Studio Code"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "myself"
+            )
+        )
+        await asyncio.sleep(999)
+        await self.client.change_presence (
+            activity = discord.Activity (
+            type = discord.ActivityType.playing, name = "discord.py"
             )
         )
 
