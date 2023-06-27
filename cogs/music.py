d2283e71eb2b1546cb6eb374927dc9c26ac87267
Elai <laiangie97@gmail.com>
2023-06-27 16:13:10 +0800

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -511,7 +511,7 @@ class Music(commands.Cog):
         )
         embed.add_field (
             name="summon",
-            value="Summons the bot to a specific voice channel when you're not in a voice channel, e.g. ```!summon General```",
+            value="Summons the bot to a specific voice channel when you're not in a voice channel, e.g. `!summon General`",
             inline=False
         )
         embed.add_field (
