74289dd60f68c94ce69d137bbe2cf10decf4dfbc
Elai <laiangie97@gmail.com>
2023-06-28 12:42:39 +0800

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -121,29 +121,6 @@ class Misc(commands.Cog):
         await ctx.send(embed=embed)
 
 
-    # welcome embed
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        if member.guild.id == 762317365970468874:
-            embed = discord.Embed (
-                title=f'Hello {member.name}, and welcome to **{member.guild.name}**!',
-                description='Have a look at the rules at <#855777347730407434> and server info at <#855775497955180545>.',
-                color=0x198C19
-            )
-            embed.set_footer(text='Thank you for joining us! We hope you enjoy your stay.')
-            await self.client.get_channel(762317365970468877).send(embed=embed)
-
-    # goodbye embed
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        if member.guild.id == 762317365970468874:
-            embed = discord.Embed (
-                title=f'{member} has left the server!',
-                description="Sorry to see you go.",
-                color=0x1e377f
-            )
-        await self.client.get_channel(762317365970468877).send(embed=embed)
-
 
 async def setup(client):
     await client.add_cog(Misc(client))
