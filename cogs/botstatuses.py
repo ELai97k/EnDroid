b89f4efa656f0854d8ca77ccf1a44e19ca1a3004
Elai <laiangie97@gmail.com>
2023-07-27 22:11:10 +0800

    Update botstatuses.py

--- cogs/botstatuses.py
+++ cogs/botstatuses.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
 
 class BotStatuses(commands.Cog):
     def __init__(self, client):
@@ -148,6 +149,34 @@ class BotStatuses(commands.Cog):
             type = discord.ActivityType.watching, name = "SPAAACE!"
             )
         )
+
+    @commands.command(help="Set custom bot status.")
+    @has_permissions(administrator=True, manage_roles=True)
+    async def change_status(self, ctx, type, newstatus=None):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+        
+        if type.lower() == "streaming":
+            await self.client.change_presence(activity=discord.Streaming(name=" development process", url=newstatus))
+        elif type.lower() == "listening":
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=newstatus))
+        elif type.lower() == "watching":
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=newstatus))
+        elif type.lower() == "playing":
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=newstatus))
+        elif type.lower() == "default":
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="!help"))
+        else:
+            await ctx.send("Invalid status! Unable to display status.")
+            return
+        await ctx.send("Changed status!")
+
+    @change_status.error
+    async def change_status_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You do not have permission to use this command!")
 
 
 async def setup(client):
