da6426c2174053aa2a818125eb018e488580810c
Elai <laiangie97@gmail.com>
2023-08-12 22:22:13 +0800

    Update botstatuses.py

--- cogs/botstatuses.py
+++ cogs/botstatuses.py
@@ -13,139 +13,19 @@ class BotStatuses(commands.Cog):
 
         await self.client.change_presence (
             activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "!help"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
             type = discord.ActivityType.playing, name = "Visual Studio Code"
             )
         )
         await asyncio.sleep(999)
         await self.client.change_presence (
             activity = discord.Activity (
-            type = discord.ActivityType.watching, name = "VLC Media Player"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "discord.py"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "Nintendo Switch"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "myself"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "El Condor Pasa"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "The Sound of Silence"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "PCSX2 1.6.0"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "Xenia Canary"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "PPSSPP"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "Drastic"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "This Wandering Day"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.watching, name = "Bohemian Rhapsody"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "Bohemian Rhapsody"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "ProPresenter"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "Where The Shadows Lie"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "VLC Media Player"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.listening, name = "VLC Media Player"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.playing, name = "SPAAACCCCCE!"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.watching, name = "The Lord of the Rings: The Rings of Power"
+            type = discord.ActivityType.playing, name = "!help"
             )
         )
         await asyncio.sleep(999)
         await self.client.change_presence (
             activity = discord.Activity (
             type = discord.ActivityType.playing, name = "on Raspberry Pi 3"
-            )
-        )
-        await asyncio.sleep(999)
-        await self.client.change_presence (
-            activity = discord.Activity (
-            type = discord.ActivityType.watching, name = "SPAAACE!"
             )
         )
 
