aa6b9cebe0fb78ba9b48761e8c7bb87540ce22d4
Elai <laiangie97@gmail.com>
2023-08-25 19:07:35 +0800

    Update auto_responses.py

--- cogs/auto_responses.py
+++ cogs/auto_responses.py
@@ -288,8 +288,12 @@ class Auto_Responses(commands.Cog):
             else:
                 if reply_message.content.lower().startswith("who made you") or reply_message.content.lower().startswith("who made u") or reply_message.content.lower().startswith("who created you") or reply_message.content.lower().startswith("who created u"):
                     creator = "<@696008187991687189>"
-                    await reply_message.channel.typing()
-                    await reply_message.channel.send(f"{creator} is my Creator.")
+                    if creator.content.lower().startswith("who made you") or reply_message.content.lower().startswith("who made u") or reply_message.content.lower().startswith("who created you") or reply_message.content.lower().startswith("who created u"):
+                        await reply_message.channel.typing()
+                        await reply_message.channel.send("You created me!")
+                    else:
+                        await reply_message.channel.typing()
+                        await reply_message.channel.send(f"{creator} is my Creator.")
 
                 # if reply_message.content.lower().startswith(""):
                 #     await reply_message.channel.typing()
