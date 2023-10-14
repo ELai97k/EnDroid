9cce3f0d1593743d06dda6c7346681ca03c8fc0a
Elai <laiangie97@gmail.com>
2023-10-14 19:53:03 +0800

    Update auto_responses.py

--- cogs/auto_responses.py
+++ cogs/auto_responses.py
@@ -3,7 +3,7 @@ import random
 import asyncio
 from discord.ext import commands
 
-class Auto_Responses(commands.Cog):
+class AutoResponses(commands.Cog):
     """Bot auto responses."""
     def __init__(self, client):
         self.client = client
@@ -334,7 +334,7 @@ class Auto_Responses(commands.Cog):
 
 
 async def setup(client):
-    await client.add_cog(Auto_Responses(client))
+    await client.add_cog(AutoResponses(client))
 
 async def teardown(client):
-    await client.remove_cog(Auto_Responses(client))
+    await client.remove_cog(AutoResponses(client))
